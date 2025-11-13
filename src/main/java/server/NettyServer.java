2876c1f2a577eaf490ba791b4cb31b74ab88fe07
abolesonike <2442168421@qq.com>
2025-11-13 15:16:45 +0800

    1.打包优化 2.端口改为9100

--- src/main/java/server/NettyServer.java
+++ src/main/java/server/NettyServer.java
@@ -48,7 +48,7 @@ public class NettyServer {
                     });//给workerGroup的EventLoop对应的管道设置处理器
             System.out.println("Netty服务端已经准备就绪...");
             //绑定端口号，启动服务端
-            ChannelFuture channelFuture = bootstrap.bind(9000).sync();
+            ChannelFuture channelFuture = bootstrap.bind(9100).sync();
             //对关闭通道进行监听
             channelFuture.channel().closeFuture().sync();
         } finally {
