5e8adc121b4eca696ca9511ef01b76c95df54acf
abolesonike <2442168421@qq.com>
2025-11-20 15:04:24 +0800

    添加data-server模块

---
+++ data-server/src/main/java/org/fizzy/dataserver/DataServerApplication.java
@@ -0,0 +1,13 @@
+package org.fizzy.dataserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataServerApplication.class, args);
+    }
+
+}
